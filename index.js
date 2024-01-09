0a501b34bc5e6c84ccc25d7420e77b90ea11b854
kristinahb06kodehode <kristinah.b06.kodehode@gmail.com>
2024-01-09 10:33:10 +0100

    litt js, addet about me, og styling

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 // Function to ease scrolling when navbar item is clicked
-
 function scrollToSection(sectionId) {
   const section = document.querySelector(sectionId);
   section.scrollIntoView({ behavior: "smooth" });
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Adding event listeners to the navigation links
+  const projectsLink = document.getElementById("projectsLink");
+  const contactLink = document.getElementById("contactLink");
+
+  projectsLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default behavior of the anchor tag
+    scrollToSection("#projects");
+  });
+
+  contactLink.addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent default behavior of the anchor tag
+    scrollToSection("#contact");
+  });
+});
